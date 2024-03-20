ea9b969a0cb58abd9758abe52b1519171ef46b9b
张亚飞 <672233580@qq.com>
2024-03-20 21:38:49 +0800

    swiper

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,5 +1,8 @@
 import 'package:get/get.dart';
 import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
+
+
 
 class HomeController extends GetxController {
   //TODO: Implement HomeController
@@ -8,22 +11,17 @@ class HomeController extends GetxController {
 
   final ScrollController scrollController = ScrollController();
 
+  var swiperlist = [].obs; //需要定义成响应式
+
   @override
   void onInit() {
     super.onInit();
-    scrollController.addListener(() {
-      if (scrollController.position.pixels > 10) {
-        if (flag.value == false) {
-          flag.value = true;
-          update();
-        }
-      }
-      if (scrollController.position.pixels <= 10) {
-        if (flag.value == true) {
-          flag.value = false;
-        }
-      }
-    });
+    
+    addScrollControllerListener();
+
+    //请求轮播图接口
+    getFocusData();
+
   }
 
   @override
@@ -35,4 +33,30 @@ class HomeController extends GetxController {
   void onClose() {
     super.onClose();
   }
+
+  void addScrollControllerListener() {
+    scrollController.addListener(() {
+
+      if (scrollController.position.pixels > 10) {
+        if (flag.value == false) {
+          flag.value = true;
+          update();
+        }
+      } 
+      if (scrollController.position.pixels <= 10) {
+        if (flag.value == true) {
+          flag.value = false;
+        }
+      }
+    });
+  }
+
+  getFocusData() async{
+    var response = await Dio().get("https://miapp.itying.com/api/focus");
+    // print(response.data);
+    swiperlist.value = response.data["result"];
+    update();
+  }
+
+
 }
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -4,74 +4,124 @@ import 'package:get/get.dart';
 import 'package:xmshop/app/services/screenAdapter.dart';
 
 import '../controllers/home_controller.dart';
-import '../../../services/screenAdapter.dart';
 import '../../../services/keepAliveWrapper.dart';
 import '../../../services/ityingFonts.dart';
 
+import 'package:flutter_swiper_view/flutter_swiper_view.dart';
+
 class HomeView extends GetView<HomeController> {
   const HomeView({Key? key}) : super(key: key);
+ 
+  Widget _appBar() {
+    return Positioned(
+      top: 0,
+      left: 0,
+      right: 0,
+      child: Obx(() => AppBar(
+            leading: controller.flag.value
+                ? const Text("")
+                : Icon(ItyingFonts.xiaomi,
+                    color: controller.flag.value == true
+                        ? Colors.black45
+                        : Colors.white),
+            leadingWidth: controller.flag.value ? 0 : ScreenAdapter.width(140),
+            title: AnimatedContainer(
+              duration: const Duration(milliseconds: 300),
+              width: controller.flag.value
+                  ? ScreenAdapter.width(800)
+                  : ScreenAdapter.width(620),
+              height: ScreenAdapter.height(96),
+              decoration: BoxDecoration(
+                color: const Color.fromARGB(230, 202, 190, 190),
+                borderRadius: BorderRadius.circular(30),
+              ),
+              child:
+                  Row(crossAxisAlignment: CrossAxisAlignment.center, children: [
+                const Padding(
+                  padding: EdgeInsets.fromLTRB(10, 0, 4, 0),
+                  child: Icon(
+                    Icons.search,
+                    color: Colors.black45,
+                  ),
+                ),
+                Text(
+                  "手机",
+                  style: TextStyle(
+                      color: Colors.black45,
+                      fontSize: ScreenAdapter.fontSize(32)),
+                ),
+              ]),
+            ),
+            centerTitle: true,
+            backgroundColor: controller.flag.value == true
+                ? Colors.white
+                : Colors.transparent,
+            elevation: 0,
+            actions: [
+              IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.qr_code,
+                    color: controller.flag.value == true
+                        ? Colors.black45
+                        : Colors.white,
+                  )),
+              IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.message,
+                    color: controller.flag.value == true
+                        ? Colors.black45
+                        : Colors.white,
+                  ))
+            ],
+          )),
+    );
+  }
+
+  Widget _homeBody() {
+    return Positioned(
+      top: -60,
+      left: 0,
+      right: 0,
+      bottom: 0,
+      //  "https://www.itying.com/images/focus/focus02.png",
+      child:ListView( 
+        controller:  controller.scrollController,
+        children: [ 
+          Container(
+            width: ScreenAdapter.width(1080),
+            height:ScreenAdapter.height(682),
+            child: Obx(() => Swiper(
+              itemBuilder:(context, index) {
+                String picUrl = "https://miapp.itying.com/${controller.swiperlist[index]["pic"]}";
+
+                return Image.network(picUrl.replaceAll("\\", "/"), fit: BoxFit.cover,);
+              },
+              itemCount: controller.swiperlist.length,
+
+              pagination: const SwiperPagination(   //指示条
+                  builder: SwiperPagination.rect
+              ),     
+              autoplay: true,
+              loop: true,
+              duration: 1000,
+              // control: const SwiperControl(), //箭头
+            )),
+          )
+        ],
+      )
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return KeepAliveWrapper(
-        child: Scaffold( 
+        child: Scaffold(
       body: Stack(
         children: [
-          Positioned(
-            top: -46,
-            left: 0,
-            right: 0,
-            bottom: 0,
-            child: ListView.builder(
-            
-            controller: controller.scrollController,  //绑定后才能监听
-              itemCount: 20,
-              itemBuilder: (context, index) {
-                if (index == 0) {
-                  return Container(
-                    width: ScreenAdapter.width(1080),
-                    height: ScreenAdapter.height(682),
-                    child: Image.network(
-                        "https://www.itying.com/images/focus/focus02.png", fit: BoxFit.cover,),
-                  );
-                } else {
-                  return ListTile(title: Text("$index  ----- 列表"));
-                }
-              }),),
-          Positioned(
-            top: 0,
-            left: 0,
-            right: 0,
-            child: Obx(() => AppBar(
-              leading: controller.flag.value ? const Text("") : Icon(
-                ItyingFonts.xiaomi,
-                color:  controller.flag.value == true ? Colors.black45 :  Colors.white
-              ),
-              leadingWidth: controller.flag.value ? 0 : ScreenAdapter.width(140),
-              title: AnimatedContainer(
-                duration: const Duration(milliseconds: 300),
-                width: controller.flag.value ? ScreenAdapter.width(800) : ScreenAdapter.width(620),
-                height: ScreenAdapter.height(96),
-                decoration: BoxDecoration( 
-                  color: const Color.fromARGB(230, 202, 190, 190),
-                  borderRadius: BorderRadius.circular(30),
-                ),
-                child:  Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children:  [ 
-                  const Padding(padding: EdgeInsets.fromLTRB(10, 0, 4, 0), child: Icon(Icons.search, color:  Colors.black45,),),
-                  Text("手机" , style: TextStyle(color: Colors.black45, fontSize:  ScreenAdapter.fontSize(32)), ),
-                  
-                ]),
-              ),
-              centerTitle: true,
-              backgroundColor: controller.flag.value == true ? Colors.white :  Colors.transparent,
-              elevation: 0,
-              actions: [
-                IconButton(onPressed: () {}, icon:  Icon(Icons.qr_code, color: controller.flag.value == true ? Colors.black45 :  Colors.white,)),
-                IconButton(onPressed: () {}, icon:  Icon(Icons.message, color: controller.flag.value == true ? Colors.black45 :  Colors.white,))
-              ],
-            )),
-          )
+          _homeBody(),
+          _appBar(),
         ],
       ),
     ));
